1c46825075c925f3f97309b2a2ef57b5ec03658f
dnomadb <damon@mapbox.com>
2015-09-23 10:57:36 -0700

    resampling w/ rasterio

---
+++ README.rst
@@ -0,0 +1,12 @@
+raster-tester
+=============
+
+::
+
+    Usage: raster-tester compare [OPTIONS] INPUT_1 INPUT_2
+
+    Options:
+      -p, --pixel-threshold INTEGER  threshold for pixel diffs
+      -r, --resample FLOAT           If the image is lossy, resample to handle
+                                     variation in compression artifacts
+      --help                         Show this message and exit.
--- raster_tester/__init__.py
+++ raster_tester/__init__.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 
+import click
+
 import numpy as np
 import rasterio as rio
-import scipy.ndimage
+from rasterio.warp import reproject, RESAMPLING
+from rasterio import Affine
+
+def affaux(up):
+    return Affine(1, 0, 0, 0, -1, 0), Affine(up, 0, 0, 0, -up, 0)
+
+def upsample(bidx, up, fr, to):
+    upBidx = np.empty((bidx.shape[0] * up, bidx.shape[1] * up), dtype=bidx.dtype)
+
+    reproject(
+        bidx, upBidx,
+        src_transform=fr,
+        dst_transform=to,
+        src_crs="EPSG:3857",
+        dst_crs="EPSG:3857",
+        resampling=RESAMPLING.bilinear)
+
+    return upBidx
 
 def compare(srcpath1, srcpath2, max_px_diff=0, resample=1):
     with rio.drivers():
@@ -21,15 +40,18 @@ def compare(srcpath1, srcpath2, max_px_diff=0, resample=1):
 
         for bidx in range(1, count1 + 1):
             band1 = src1.read(bidx, masked=False).astype(np.int16)
-            band1 = scipy.ndimage.zoom(band1, float(resample), order=1)
             band2 = src2.read(bidx, masked=False).astype(np.int16)
-            band2 = scipy.ndimage.zoom(band2, float(resample), order=1)
+
+            if resample > 1:
+                toAff, frAff = affaux(resample)
+                band1 = upsample(band1, resample, frAff, toAff)
+                band2 = upsample(band2, resample, frAff, toAff)
 
             diff = np.absolute(band1 - band2)
             threshold = np.zeros(band1.shape)
             outliers = np.where(diff > 16)
             if outliers[0].size > max_px_diff:
-                print outliers[0]
+                click.echo(outliers[0], err=True)
             assert outliers[0].size <= max_px_diff, "band %s has %d pixels which differ by > 16" % (bidx, outliers[0].size)
 
         src1.close()
