b45a2e24b52eb4bec38ea77262c2096b3048bd3e
dnomadb <damon@mapbox.com>
2015-10-01 16:41:09 -0400

    bumpin v + update reame

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='raster-tester',
-      version='0.0.1',
+      version='0.1.0',
       description=u"Tools for testing rasters",
       long_description=long_description,
       classifiers=[],
