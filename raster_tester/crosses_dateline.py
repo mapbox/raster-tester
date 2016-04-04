5411abe19b28c8d32e757521500ae149f7900ac6
dnomadb <damon@mapbox.com>
2016-04-03 21:08:19 -0700

    cleanup

--- raster_tester/crosses_dateline.py
+++ raster_tester/crosses_dateline.py
@@ -1,7 +1,7 @@
 import rasterio as rio
 from rasterio import warp
 import numpy as np
-from matplotlib.pyplot import plot, show
+
 
 def winding_order(boundsArr):
     '''
@@ -9,16 +9,20 @@ def winding_order(boundsArr):
     (ie, it crosses the dateline and is inverted when unprojected)
     '''
     EPSLN = 1.0e-10
-    ## Add EPSLN to the first point to catch exact rectangles
+    # Add EPSLN to the first point to catch exact rectangles
     boundsArr[0] += EPSLN
     delta = np.roll(boundsArr, -1, 0) - boundsArr
     return np.sum(delta[:, 0] * delta[:, 1]) > 0
+
 
 def densify(boundsArr, dens=4):
     '''
     Adds points between the bounds corners
     '''
-    denseBounds = np.zeros(((boundsArr.shape[0]) * dens, 2), dtype=boundsArr.dtype)
+    denseBounds = np.zeros(
+        ((boundsArr.shape[0]) * dens, 2),
+        dtype=boundsArr.dtype)
+
     denseBounds[::dens] += boundsArr
     diffs = (boundsArr - np.roll(boundsArr, -1, 0)) / float(dens)
     for i in range(1, dens):
@@ -35,11 +39,17 @@ def make_bounds_array(bounds):
         [bounds.left, bounds.top]
         ])
 
+
 def crosses_dateline(fpath):
     '''
     Checks if a raster crosses the dateline after unprojection to epsg 4326
     '''
     with rio.open(fpath) as src:
         denseBounds = densify(make_bounds_array(src.bounds))
-        unprojectedBounds = warp.transform(src.crs, {'init': 'epsg:4326'}, denseBounds[:, 0], denseBounds[:, 1])
+        unprojectedBounds = warp.transform(
+            src.crs,
+            {'init': 'epsg:4326'},
+            denseBounds[:, 0],
+            denseBounds[:, 1])
+
         return winding_order(np.dstack(unprojectedBounds)[0])
