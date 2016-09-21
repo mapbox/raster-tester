f8e4c9e7d4ccea20bc0c1ea44906cead899370af
Matthew Perry <perrygeo@gmail.com>
2016-09-21 09:21:09 -0400

    turn off implicit jpeg overviews, gdal2 compat

--- raster_tester/compare.py
+++ raster_tester/compare.py
@@ -93,91 +93,92 @@ def compare_properties(src1, src2, properties, tol=1e-6):
 
 def compare(srcpath1, srcpath2, max_px_diff=0, upsample=1, downsample=1,
             compare_masked=True, no_stderr=False, debug=False, flex_mode=False):
-    with rasterio.open(srcpath1) as src1:
-        with rasterio.open(srcpath2) as src2:
+    with rasterio.Env(GTIFF_IMPLICIT_JPEG_OVR=False):
+        with rasterio.open(srcpath1) as src1:
+            with rasterio.open(srcpath2) as src2:
 
-            count1 = src1.count
-            count2 = src2.count
-            compareAlpha = 1
+                count1 = src1.count
+                count2 = src2.count
+                compareAlpha = 1
 
-            if flex_mode and [count1, count2].count(3) != 0:
-                props = ['crs', 'driver', 'bounds', 'height', 'width', 'shape']
-                if src1.count * src2.count != 12 or src1.count + src2.count != 7:
-                    exception_raiser(
-                        "In flex mode, %s and %s must 3 and 4, or 4 and 3 bands "
-                        "respectively (received %s and %s)" % (
-                            srcpath1, srcpath2, src1.count, src2.count), no_stderr)
-            else:
-                props = ['count', 'crs', 'dtypes', 'driver', 'bounds',
-                         'height', 'width', 'shape', 'nodatavals']
-            propCompare = compare_properties(src1, src2, props)
+                if flex_mode and [count1, count2].count(3) != 0:
+                    props = ['crs', 'driver', 'bounds', 'height', 'width', 'shape']
+                    if src1.count * src2.count != 12 or src1.count + src2.count != 7:
+                        exception_raiser(
+                            "In flex mode, %s and %s must 3 and 4, or 4 and 3 bands "
+                            "respectively (received %s and %s)" % (
+                                srcpath1, srcpath2, src1.count, src2.count), no_stderr)
+                else:
+                    props = ['count', 'crs', 'dtypes', 'driver', 'bounds',
+                            'height', 'width', 'shape', 'nodatavals']
+                propCompare = compare_properties(src1, src2, props)
 
-            if propCompare:
-                exception_raiser(propCompare, no_stderr)
+                if propCompare:
+                    exception_raiser(propCompare, no_stderr)
 
-            if compare_masked and src1.count == 4 and not flex_mode:
-                # create arrays for decimated reading
-                masked_1 = make_fill_array(
-                    src1.height, src1.width, downsample, src1.meta['dtype'])
-                masked_2 = make_fill_array(
-                    src2.height, src2.width, downsample, src2.meta['dtype'])
+                if compare_masked and src1.count == 4 and not flex_mode:
+                    # create arrays for decimated reading
+                    masked_1 = make_fill_array(
+                        src1.height, src1.width, downsample, src1.meta['dtype'])
+                    masked_2 = make_fill_array(
+                        src2.height, src2.width, downsample, src2.meta['dtype'])
 
-                src1.read(4, out=masked_1, masked=False)
-                src2.read(4, out=masked_2, masked=False)
-                compareAlpha = 0
-                difference, aboveThreshold = array_compare(
-                    masked_1, masked_2, 16, max_px_diff, debug)
+                    src1.read(4, out=masked_1, masked=False)
+                    src2.read(4, out=masked_2, masked=False)
+                    compareAlpha = 0
+                    difference, aboveThreshold = array_compare(
+                        masked_1, masked_2, 16, max_px_diff, debug)
 
-                if aboveThreshold:
-                    exception_raiser(
-                        'Mask has %s pixels that vary by more than 16' % (difference), no_stderr)
+                    if aboveThreshold:
+                        exception_raiser(
+                            'Mask has %s pixels that vary by more than 16' % (difference), no_stderr)
 
-            elif compare_masked and flex_mode:
-                masked_1 = make_fill_array(
-                    src1.height, src1.width, downsample, src1.meta['dtype'])
-                masked_2 = make_fill_array(
-                    src2.height, src2.width, downsample, src2.meta['dtype'])
+                elif compare_masked and flex_mode:
+                    masked_1 = make_fill_array(
+                        src1.height, src1.width, downsample, src1.meta['dtype'])
+                    masked_2 = make_fill_array(
+                        src2.height, src2.width, downsample, src2.meta['dtype'])
 
-                src1.read_masks(1, out=masked_1)
-                src2.read_masks(1, out=masked_2)
-                compareAlpha = 0
+                    src1.read_masks(1, out=masked_1)
+                    src2.read_masks(1, out=masked_2)
+                    compareAlpha = 0
 
-                difference, aboveThreshold = array_compare(
-                    masked_1, masked_2, 16, max_px_diff, debug)
+                    difference, aboveThreshold = array_compare(
+                        masked_1, masked_2, 16, max_px_diff, debug)
 
-                if aboveThreshold:
-                    exception_raiser(
-                        'Mask has %s pixels that vary by more than 16' % (difference),
-                        no_stderr)
+                    if aboveThreshold:
+                        exception_raiser(
+                            'Mask has %s pixels that vary by more than 16' % (difference),
+                            no_stderr)
 
-            for bidx in range(1, count1 + compareAlpha):
-                # create arrays for decimated reading
-                band1 = make_fill_array(
-                    src1.height, src1.width, downsample, src1.meta['dtype'])
-                band2 = make_fill_array(
-                    src2.height, src2.width, downsample, src2.meta['dtype'])
+                for bidx in range(1, count1 + compareAlpha):
+                    # create arrays for decimated reading
+                    band1 = make_fill_array(
+                        src1.height, src1.width, downsample, src1.meta['dtype'])
+                    band2 = make_fill_array(
+                        src2.height, src2.width, downsample, src2.meta['dtype'])
 
-                src1.read(bidx, out=band1, masked=False)
-                band1 = band1.astype(np.int16)
+                    src1.read(bidx, out=band1, masked=False)
+                    band1 = band1.astype(np.int16)
 
-                src2.read(bidx, out=band2, masked=False)
-                band2 = band2.astype(np.int16)
+                    src2.read(bidx, out=band2, masked=False)
+                    band2 = band2.astype(np.int16)
 
-                if compare_masked and src1.count == 4:
-                    band1[masked_1 != 255] = 0
-                    band2[masked_1 != 255] = 0
+                    if compare_masked and src1.count == 4:
+                        band1[masked_1 != 255] = 0
+                        band2[masked_1 != 255] = 0
 
-                if upsample > 1:
-                    toAff, frAff = affaux(upsample)
-                    band1 = upsample_array(band1, upsample, frAff, toAff)
-                    band2 = upsample_array(band2, upsample, frAff, toAff)
+                    if upsample > 1:
+                        toAff, frAff = affaux(upsample)
+                        band1 = upsample_array(band1, upsample, frAff, toAff)
+                        band2 = upsample_array(band2, upsample, frAff, toAff)
 
-                difference, aboveThreshold = array_compare(
-                    band1, band2, 16, max_px_diff, debug)
+                    difference, aboveThreshold = array_compare(
+                        band1, band2, 16, max_px_diff, debug)
 
-                if aboveThreshold:
-                    exception_raiser('Band %s has %s pixels that vary by more than 16' % (
-                        bidx, difference), no_stderr)
+                    if aboveThreshold:
+                        exception_raiser('Band %s has %s pixels that vary by more than 16' % (
+                            bidx, difference), no_stderr)
 
     click.echo("ok - %s is similar to within %s pixels of %s" %
                (srcpath1, max_px_diff, srcpath2))
