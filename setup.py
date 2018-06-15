54b8fba94ff1a5d2d309497969bfab4f9b1daf09
vincentsarago <vincent.sarago@mapbox.com>
2018-06-15 17:22:29 -0400

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='raster-tester',
-      version='0.10.0',
+      version='0.11.0',
       description=u"Tools for testing rasters",
       long_description=long_description,
       classifiers=[
